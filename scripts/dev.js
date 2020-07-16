fc449240c55951cb3284d28af9b2b03dc2a9a5da
edgar <15989069487@163.com>
2020-07-16 11:09:00 +0800

    docs: 注释scripts/dev.js

--- scripts/dev.js
+++ scripts/dev.js
@@ -18,12 +18,22 @@ __DEV__=false yarn dev
 
 const execa = require('execa')
 const { fuzzyMatchTarget } = require('./utils')
+// 解析命令行参数输出一个对象
 const args = require('minimist')(process.argv.slice(2))
+// args._是一个数组，包含未指定参数名的变量。
+// 如 // node scripts/dev.js template-explorer args._ 为['template-explorer']
+// target是packages目录下的符合条件的目录名
 const target = args._.length ? fuzzyMatchTarget(args._)[0] : 'vue'
 const formats = args.formats || args.f
 const sourceMap = args.sourcemap || args.s
+// 截取前7位作为commit id
 const commit = execa.sync('git', ['rev-parse', 'HEAD']).stdout.slice(0, 7)
 
+// 终端命令：rollup -wc --environment COMMIT:{commit},TARGET:{target},FORMATS:{formats},SOURCE_MAP:true?
+// -w watch file change
+// -c 通过配置文件配置，不设置配置文件则是默认的rollup.config.js
+// --environment <values> 将额外的变量传入配置文件中，通过process.env.XXX 读取
+// https://rollupjs.org/guide/en/#command-line-flags
 execa(
   'rollup',
   [
@@ -35,10 +45,10 @@ execa(
       `FORMATS:${formats || 'global'}`,
       sourceMap ? `SOURCE_MAP:true` : ``
     ]
-      .filter(Boolean)
+      .filter(Boolean) // 过滤掉假的值。Boolean将接收到的第一个参数转为boolean类型值
       .join(',')
   ],
   {
-    stdio: 'inherit'
+    stdio: 'inherit' // https://nodejs.org/api/child_process.html#child_process_options_stdio TODO 还不清楚
   }
 )
